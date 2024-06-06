81f2904b2bfe015bc6a79cb7698053ae86c67ce2
nabicold03 <binh.vunabi2509@hcmut.edu.vn>
2024-06-06 13:04:08 +0700

    First Commit

---
+++ SportWatch/SportWatch/SportWatch.srcs/sources_1/new/SportWatch.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/06/2024 01:03:30 PM
+// Design Name: 
+// Module Name: SportWatch
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module SportWatch(
+
+    );
+endmodule
